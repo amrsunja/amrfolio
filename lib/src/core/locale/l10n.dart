e785312f472d952f59e4ee8f9ddad6689d1ef7e0
amrsunja <amrsunja@air-amrsunja.home>
2024-12-21 19:20:39 +0100

    ❇️ Created app localization feature

---
+++ lib/src/core/locale/l10n.dart
@@ -0,0 +1,40 @@
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+typedef AppLocale = AppLocalizations;
+
+final l10nProvider = Provider<AppLocale>((ref) {
+	// 1. initialize from the initial locale
+	final locale = L10n.defaultLocale ;
+
+	ref.state = lookupAppLocalizations(locale);
+	// 2. create an observer to update the state
+	//TODO: Uncomment this code line after creating localSettingsProvider
+	//final localSettings = ref.watch(localSettingsProvider).data;
+
+
+	//TODO: Uncomment this code line after creating localSettingsProvider
+	//ref.state = lookupAppLocalizations(localSettings?.locale ?? locale);
+	ref.state = lookupAppLocalizations(locale);
+
+	return ref.state;
+});
+
+
+class L10n {
+	static Locale get rus => all[0];
+	static Locale get eng => all[1];
+	static Locale get ing => all[2];
+
+  static final all = [
+    const Locale('en', 'US'),
+		///This is Galician language, but for us it is Galga language ;)
+    const Locale('fr', "FR"),
+  ];
+
+	static Locale get defaultLocale => all.contains(ui.PlatformDispatcher.instance.locale)
+		? ui.PlatformDispatcher.instance.locale : rus;
+}
+
