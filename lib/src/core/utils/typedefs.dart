2e271d2d6ed6ff7dd0f76392bf1a208aa96da376
amrsunja <amrsunja@air-amrsunja.home>
2024-12-22 03:23:52 +0100

    🔧 added typedefs

---
+++ lib/src/core/utils/typedefs.dart
@@ -0,0 +1,7 @@
+typedef Json = Map<String, dynamic>;
+typedef JsonList = List<Map<String, dynamic>>;
+/// Params type when we send http request
+typedef QueryParams = Map<String, dynamic>;
+
+/// Error = Failure | Success = <T> | return failure or a <T>
+//typedef SuccessOrError<T> = Result<T, Failure>;
