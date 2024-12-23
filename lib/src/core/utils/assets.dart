d981b257c619e913287445bbee708c23b1bb0855
amrsunja <amrsunja@air-amrsunja.home>
2024-12-23 18:03:58 +0100

    🍱 added new asset and constants values

---
+++ lib/src/core/utils/assets.dart
@@ -0,0 +1,5 @@
+class Assets {
+	static const String towns = 'assets/images/towns.png';
+	static const String birds = 'assets/images/birds.png';
+	static const String sphetofor = 'assets/images/sphetofor.png';
+}
