270772ba5d448cc9c62afe5ebe96f87d19a452bf
amrsunja <amrsunja@air-amrsunja.home>
2024-12-25 16:57:11 +0100

    🍱added new assets

--- lib/src/core/utils/assets.dart
+++ lib/src/core/utils/assets.dart
@@ -2,4 +2,6 @@ class Assets {
 	static const String towns = 'assets/images/towns.png';
 	static const String birds = 'assets/images/birds.png';
 	static const String sphetofor = 'assets/images/sphetofor.png';
+	static const String drop = 'assets/images/drop.png';
+	static const String drawnAvatar = 'assets/images/drawn_avatar.png';
 }
